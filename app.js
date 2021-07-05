e6a706ed5e3a2dc432120ebfa684ef8ecdf11e5a
Anil <dangolanil23@gmail.com>
2021-07-05 16:05:00 -0700

    restrict colors to purple

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 function randomRGB() {
     const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
+    const g = 0
     const b = Math.floor(Math.random() * 256);
-    return `rgb(${r},${g},${b})`
+    return `rgb(${r},0,${b})`
 }
 
 const letters = document.querySelectorAll('.letter');
